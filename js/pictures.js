49972d22e58b34ab20f8311a4fa09b261cb7039b
Anna <anna.mironova89@yandex.ru>
2023-10-22 12:34:25 +0300

    8.14. Открывается и закрывается

--- js/pictures.js
+++ js/pictures.js
@@ -1,4 +1,5 @@
 import {generatePhotos} from './data.js';
+import {openFullPhoto} from './full-photo.js';
 
 const pictures = generatePhotos();
 
@@ -6,14 +7,22 @@ const container = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const fragment = document.createDocumentFragment();
 
+const renderPicture = (picture) => {
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = picture.url;
+  pictureElement.querySelector('.picture__img').alt = picture.description;
+  pictureElement.querySelector('.picture__likes').textContent = picture.likes;
+  pictureElement.querySelector('.picture__comments').textContent = picture.comments.length;
+  pictureElement.addEventListener('click', () => {
+    openFullPhoto(picture);
+  });
+  return pictureElement;
+};
+
 const renderPictures = () => {
-  pictures.forEach(({url, description, likes, comments}) => {
-    const picture = pictureTemplate.cloneNode(true);
-    picture.querySelector('.picture__img').src = url;
-    picture.querySelector('.picture__img').alt = description;
-    picture.querySelector('.picture__likes').textContent = likes;
-    picture.querySelector('.picture__comments').textContent = comments.length;
-    fragment.append(picture);
+  pictures.forEach((picture) => {
+    const pictureElement = renderPicture(picture);
+    fragment.append(pictureElement);
   });
   container.append(fragment);
 };
