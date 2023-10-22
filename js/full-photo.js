1bb6a4ba2b29cacf9096667cb1c59ea6a9ffcd8a
Anna <anna.mironova89@yandex.ru>
2023-10-22 15:57:30 +0300

    8.15. Открывается и закрывается (часть 2)

--- js/full-photo.js
+++ js/full-photo.js
@@ -1,6 +1,9 @@
+const COMMENTS_COUNT_AT_ONCE = 5;
+
 const fullPhoto = document.querySelector('.big-picture');
 const closeBtn = document.querySelector('.big-picture__cancel');
 const commentsList = fullPhoto.querySelector('.social__comments');
+const loadBtn = fullPhoto.querySelector('.comments-loader');
 
 const renderComment = ({avatar, name, message}) => `
   <li class="social__comment">
@@ -13,31 +16,50 @@ const renderComment = ({avatar, name, message}) => `
   </li>
 `;
 
-const renderComments = (comments) => {
-  commentsList.innerHTML = '';
-
-  comments.forEach((item) => {
-    const comment = renderComment(item);
+const renderCurrentComments = (maxIndex, comments) => {
+  let currentIndex = commentsList.children.length > 0 ? commentsList.children.length : 0;
+  if (maxIndex >= comments.length) {
+    maxIndex = comments.length;
+    loadBtn.classList.add('hidden');
+  }
+  while (currentIndex < maxIndex) {
+    const comment = renderComment(comments[currentIndex]);
     commentsList.insertAdjacentHTML('beforeend', comment);
-  });
+    currentIndex++;
+  }
+};
+
+let loadCommentsOnBtn;
+
+const renderComments = (comments) => {
+  let maxIndex = COMMENTS_COUNT_AT_ONCE;
+
+  loadCommentsOnBtn = () => {
+    renderCurrentComments(maxIndex, comments);
+    fullPhoto.querySelector('.social__comment-shown-count').textContent = maxIndex > comments.length ? comments.length : maxIndex;
+    maxIndex += COMMENTS_COUNT_AT_ONCE;
+  };
+
+  commentsList.innerHTML = '';
+  loadBtn.classList.remove('hidden');
+
+  loadCommentsOnBtn();
+
+  loadBtn.addEventListener('click', loadCommentsOnBtn);
 };
 
 const renderFullPhoto = ({url, likes, comments, description}) => {
   fullPhoto.querySelector('img').src = url;
   fullPhoto.querySelector('.likes-count').textContent = likes;
-  fullPhoto.querySelector('.social__comment-shown-count').textContent = comments.length;
   fullPhoto.querySelector('.social__comment-total-count').textContent = comments.length;
   fullPhoto.querySelector('.social__caption').textContent = description;
 
   renderComments(comments);
-
-  fullPhoto.querySelector('.social__comment-count').classList.add('hidden');
-  fullPhoto.querySelector('.comments-loader').classList.add('hidden');
 };
 
 const closeFullPhoto = () => {
+  loadBtn.removeEventListener('click', loadCommentsOnBtn);
   fullPhoto.classList.add('hidden');
-
   document.body.classList.remove('modal-open');
   closeBtn.removeEventListener('click', closeFullPhoto);
   document.removeEventListener('keydown', closeFullPhotoByEscape);
